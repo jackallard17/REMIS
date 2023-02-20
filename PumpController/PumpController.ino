4a8bc62772fd29bcab5df3816bc1616d857bda4c
Jack Allard <jackallard17@gmail.com>
2023-02-19 21:18:33 -0800

    moved pumpcontroller into directory for arduino sketch compatability

--- PumpController/PumpController.ino
+++ PumpController/PumpController.ino
@@ -1,5 +1,18 @@
-#include <cstdint>
+#include <LiquidCrystal.h>
+#include <stdint.h>
+#include <Wire.h>
 using namespace std;
+
+int LCD_SDA = 6;
+
+
+void setup() {
+
+}
+
+void loop () {
+
+}
 
 uint8_t mushroom[8] = {
     0b00000,
