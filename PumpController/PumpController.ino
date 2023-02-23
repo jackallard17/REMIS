3fc1df824da764f1c78f1d4553922958ef9c06cb
Jack Allard <jackallard17@gmail.com>
2023-02-23 15:05:19 -0800

    LCD screen initialization and welcome message

--- PumpController/PumpController.ino
+++ PumpController/PumpController.ino
@@ -4,29 +4,20 @@
 
 using namespace std;
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C lcd(0x27, 2, 16);
 
-//Digital pins D2 - D7
+// Digital pins D2 - D7
 #define ROT_CLK 5
-#define ROT_DT 6
-#define ROT_SW 7
+#define ROT_DT 6 //rotary encoder direction
+#define ROT_SW 7 //rotary encoder switch (press in)
 #define STEPPER_DIR 8
 #define STEPPER_STEP 9
 
-//Analog pins A4 - A7. The LCD pins, (23 and 24) will be set to digital mode.
+// Analog pins A4 - A7. The LCD pins, (23 and 24) will be set to digital mode.
 #define LCD_SDA 23
 #define LCD_SDL 24
 #define TRIGGER 25
 #define TOGGLE 26
-
-void setup() {
-
-
-}
-
-void loop () {
-
-}
 
 uint8_t mushroom[8] = {
     0b00000,
@@ -37,3 +28,56 @@ uint8_t mushroom[8] = {
     0b00100,
     0b00100,
     0b00100};
+
+void setup()
+{
+    //LCD init and startup message
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.createChar(0, mushroom); 
+    lcd.setCursor(0, 0);
+    lcd.print("REMIS       v0.0");
+    lcd.setCursor(0, 12);
+    drawMushrooms();
+
+}
+
+void loop()
+{
+}
+
+void drawMushrooms(){
+    lcd.setCursor(0,1);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+    delay(100);
+    lcd.write((byte)0);
+}
