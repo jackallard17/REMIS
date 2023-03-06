be34eaec296790c96598c1a5db9bfcf6d7bd16b3
Jack Allard <jackallard17@gmail.com>
2023-03-06 15:43:28 -0800

    added debug mode for viewing i/o states on lcd screen

--- PumpController/PumpController.ino
+++ PumpController/PumpController.ino
@@ -1,3 +1,4 @@
+#include <Encoder.h>
 #include <LiquidCrystal_I2C.h>
 #include <stdint.h>
 #include <Wire.h>
@@ -5,19 +6,27 @@
 using namespace std;
 
 LiquidCrystal_I2C lcd(0x27, 2, 16);
+Encoder encoder(5,6);
+
+long prevPosition;
 
 // Digital pins D2 - D7
-#define ROT_CLK 5
-#define ROT_DT 6 //rotary encoder direction
-#define ROT_SW 7 //rotary encoder switch (press in)
+#define ROT_CLK 5 // rotary encoder clock
+#define ROT_DT 6 // rotary encoder direction
+#define ROT_SW 7 // rotary encoder switch (press in)
 #define STEPPER_DIR 8
 #define STEPPER_STEP 9
 
 // Analog pins A4 - A7. The LCD pins, (23 and 24) will be set to digital mode.
-#define LCD_SDA 23
-#define LCD_SDL 24
-#define TRIGGER 25
-#define TOGGLE 26
+// #define LCD_SDA 23
+// #define LCD_SDL 24
+#define LCD_SDA 22
+#define LCD_SDL 21
+#define TRIGGER 20
+#define TOGGLE 10
+
+//threshold voltages to parse analog signal
+int triggerThreshold;
 
 uint8_t mushroom[8] = {
     0b00000,
@@ -31,64 +40,113 @@ uint8_t mushroom[8] = {
 
 void setup()
 {
-    //LCD init and startup message
-    lcd.init();
-    lcd.backlight();
-    lcd.setCursor(0, 0);
-    lcd.createChar(0, mushroom); 
-    lcd.setCursor(0, 0);
-    lcd.print("REMIS       v0.0");
-    lcd.setCursor(0, 12);
-    drawMushrooms();
+  // LCD init and startup message
+  lcd.init();
+  lcd.backlight();
+  lcd.createChar(0, mushroom);
+  lcd.setCursor(0, 0);
+  lcd.print("REMIS       v0.0");
+  lcd.setCursor(0, 12);
+  drawMushrooms();
 
-    //inputs
-    pinMode(TRIGGER, INPUT);
-    pinMode(TOGGLE, INPUT);
-    pinMode(ROT_CLK, INPUT);
-    pinMode(ROT_DT, INPUT);
-    pinMode(ROT_SW, INPUT);
+  // inputs
+  pinMode(TRIGGER, INPUT);
+  pinMode(TOGGLE, INPUT);
+  pinMode(ROT_CLK, INPUT);
+  pinMode(ROT_DT, INPUT);
+  pinMode(ROT_SW, INPUT);
 
-    //outputs
-    pinMode(STEPPER_DIR, OUTPUT);
-    pinMode(STEPPER_STEP, OUTPUT);
+  // outputs
+  pinMode(STEPPER_DIR, OUTPUT);
+  pinMode(STEPPER_STEP, OUTPUT);
+
+  Serial.begin(9600);
+
+  lcd.clear();
 }
+
 
 void loop()
 {
+  debugMode();
+
+  // long newPosition = encoder.read();
+
+  // if (newPosition != prevPosition)
+  // {
+  //   prevPosition= newPosition;
+  //   Serial.println(newPosition);
+  //   lcd.print(newPosition);
+  // }
+
+  // if (analogRead(TRIGGER) > 1000) 
+  // {
+  //   lcd.print("trigger pressed");
+  // } else 
+  // {
+  //   lcd.clear();
+  // }
+}
+
+void debugMode()
+{
+  while(true)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T:");
+    // lcd.print(digitalRead(TOGGLE));
+    lcd.print(digitalRead(10));
+    lcd.setCursor(9, 0);
+    lcd.print("Tr:");
+    lcd.print(analogRead(TRIGGER));
+
+    lcd.setCursor(0, 1);
+    lcd.print("RC:");
+    lcd.print(digitalRead(ROT_CLK));
+    lcd.print(digitalRead(ROT_DT));
+    lcd.print(digitalRead(ROT_SW));
+    lcd.setCursor(9, 1);
+    lcd.print("RP:");
+    lcd.print(encoder.read());
+
+    delay(100);
+  }
 
 }
 
-void drawMushrooms(){
-    lcd.setCursor(0,1);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
-    delay(100);
-    lcd.write((byte)0);
+void drawMushrooms()
+{
+  lcd.setCursor(0, 1);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
+  delay(100);
+  lcd.write((byte)0);
 }
