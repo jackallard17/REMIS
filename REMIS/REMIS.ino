12e6957de066153fb12ea01af767205dc83a61da
Jack Allard <jackallard17@gmail.com>
2024-06-19 13:25:53 -0700

    re-integrate legacy pump running message

--- REMIS/REMIS.ino
+++ REMIS/REMIS.ino
@@ -2,54 +2,54 @@
 
 #ifndef ARDUINO_SAM_DUE
 
-  #include <Wire.h>
-  #include <LiquidCrystal_I2C.h>//F. Malpartida LCD's driver
-  #include <menu.h>//menu macros and objects
-  #include <menuIO/lcdOut.h>//malpartidas lcd menu output
-  #include <menuIO/serialIn.h>//Serial input
-  #include <menuIO/encoderIn.h>//quadrature encoder driver and fake stream
-  #include <menuIO/keyIn.h>//keyboard driver and fake stream (for the encoder button)
-  #include <menuIO/chainStream.h>// concatenate multiple input streams (this allows adding a button to the encoder)
-  #include <Stepper.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>  //F. Malpartida LCD's driver
+#include <menu.h>               //menu macros and objects
+#include <menuIO/lcdOut.h>      //malpartidas lcd menu output
+#include <menuIO/serialIn.h>    //Serial input
+#include <menuIO/encoderIn.h>   //quadrature encoder driver and fake stream
+#include <menuIO/keyIn.h>       //keyboard driver and fake stream (for the encoder button)
+#include <menuIO/chainStream.h> // concatenate multiple input streams (this allows adding a button to the encoder)
+#include <Stepper.h>
 
-  // globals
-  #define STEPPER_STEP 5
-  #define STEPPER_DIR 6
+// globals
+#define STEPPER_STEP 5
+#define STEPPER_DIR 6
 
-  // Digital pins D2 - D7
-  #define ROT_CLK 2 // rotary encoder clock
-  #define ROT_DT 3  // rotary encoder direction
-  #define ROT_SW 4  // rotary encoder switch (press in)
+// Digital pins D2 - D7
+#define ROT_CLK 2 // rotary encoder clock
+#define ROT_DT 3  // rotary encoder direction
+#define ROT_SW 4  // rotary encoder switch (press in)
 
-  #define LCD_SDA 22
-  #define LCD_SDL 21
+#define LCD_SDA 22
+#define LCD_SDL 21
 
-  #define TOGGLESWITCH 7
-  #define TRIGGER 8
+#define TOGGLESWITCH 7
+#define TRIGGER 8
 
-  #define STEPS 200
+#define STEPS 200
 
-  #define MAX_DEPTH 2
+#define MAX_DEPTH 2
 
-  volatile bool pumpRunning = false;
-  volatile bool dashboardDisplayed = true;
-  volatile bool frequencyUpdated = false;
+volatile bool pumpRunning = false;
+volatile bool dashboardDisplayed = true;
+volatile bool frequencyUpdated = false;
 
-  float period;
-  long step_delay_microseconds;
+float period;
+long step_delay_microseconds;
 
-  float ml_per_rev = 200;
+float ml_per_rev = 200;
 
-  volatile int last_CLK_state = LOW;
-  volatile int last_DT_state = LOW;
+volatile int last_CLK_state = LOW;
+volatile int last_DT_state = LOW;
 
-  Stepper stepper(STEPS, STEPPER_DIR, STEPPER_STEP);
+Stepper stepper(STEPS, STEPPER_DIR, STEPPER_STEP);
 
-  // persistent properties, stored between power cycles
-  float frequency;  // frequency of the pump in Hz
-  int dose;         // dose amount in mL
+// persistent properties, stored between power cycles
+float frequency; // frequency of the pump in Hz
+int dose;        // dose amount in mL
 
-  uint8_t mushroom[8] = {
+uint8_t mushroom[8] = {
     0b00000,
     0b00100,
     0b01110,
@@ -57,179 +57,166 @@
     0b00100,
     0b00100,
     0b00100,
-    0b00100
-};
+    0b00100};
 
 byte batteryIcon[8] = {
-  B00100,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B00000
+    B00100,
+    B01110,
+    B01110,
+    B01110,
+    B01110,
+    B01110,
+    B01110,
+    B00000};
+
+using namespace Menu;
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address and pinout
+
+// Encoder
+#define encA 2
+#define encB 3
+#define encBtn 4
+
+encoderIn<encA, encB> encoder;
+#define ENC_SENSIVITY 4
+encoderInStream<encA, encB> encStream(encoder, ENC_SENSIVITY);
+
+keyMap encBtn_map[] = {{-encBtn, defaultNavCodes[enterCmd].ch}};
+keyIn<1> encButton(encBtn_map);
+
+serialIn serial(Serial);
+
+// input from the encoder + encoder button + serial
+menuIn *inputsList[] = {&encStream, &encButton, &serial};
+chainStream<3> in(inputsList); // 3 is the number of inputs
+
+result doCalibration(eventMask e, prompt &item);
+
+result showEvent(eventMask e, navNode &nav, prompt &item)
+{
+  Serial.print("event: ");
+  Serial.println(e);
+  return proceed;
+}
+
+int test = 420;
+
+result action1(eventMask e, navNode &nav, prompt &item)
+{
+  Serial.print("action1 event: ");
+  Serial.print(e);
+  Serial.println(", proceed menu");
+  Serial.flush();
+  return proceed;
+}
+
+result action2(eventMask e, navNode &nav, prompt &item)
+{
+  Serial.print("action2 event: ");
+  Serial.print(e);
+  Serial.println(", quiting menu.");
+  Serial.flush();
+  return quit;
+}
+
+int injectorMode = 1;
+
+TOGGLE(injectorMode, setInjectorMode, "Mode: ", doNothing, noEvent, noStyle //,doExit,enterEvent,noStyle
+       ,
+       VALUE("Dose", 0, doNothing, noEvent), VALUE("Cont.", 1, doNothing, noEvent), VALUE("Toggle.", 2, doNothing, noEvent));
+
+int selTest = 0;
+SELECT(selTest, selMenu, "Select", doNothing, noEvent, noStyle, VALUE("Zero", 0, doNothing, noEvent), VALUE("One", 1, doNothing, noEvent), VALUE("Two", 2, doNothing, noEvent));
+
+int chooseTest = -1;
+CHOOSE(chooseTest, chooseMenu, "Choose", doNothing, noEvent, noStyle, VALUE("First", 1, doNothing, noEvent), VALUE("Second", 2, doNothing, noEvent), VALUE("Third", 3, doNothing, noEvent), VALUE("Last", -1, doNothing, noEvent));
+
+class altPrompt : public prompt
+{
+public:
+  altPrompt(constMEM promptShadow &p) : prompt(p) {}
+  Used printTo(navRoot &root, bool sel, menuOut &out, idx_t idx, idx_t len, idx_t) override
+  {
+    return out.printRaw(F("special prompt!"), len);
+    ;
+  }
 };
 
-  using namespace Menu;
+MENU(subMenu, "", showEvent, anyEvent, noStyle, OP("Sub1", showEvent, anyEvent), OP("Sub2", showEvent, anyEvent), OP("Sub3", showEvent, anyEvent), altOP(altPrompt, "", showEvent, anyEvent), EXIT("<Back"));
 
-  LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address and pinout
+char *constMEM hexDigit MEMMODE = "0123456789ABCDEF";
+char *constMEM hexNr[] MEMMODE = {"0", "x", hexDigit, hexDigit};
+char buf1[] = "0x11";
 
-  // Encoder
-  #define encA 2
-  #define encB 3
-  #define encBtn 4
+MENU(mainMenu, "Main menu", doNothing, noEvent, noStyle, FIELD(test, "Speed: ", "RPM", 0, 500, 10, 1, doNothing, noEvent, noStyle), OP("================", doNothing, enterEvent), SUBMENU(setInjectorMode), OP("Calibrate", doCalibration, enterEvent), EXIT("<Back"));
 
-  encoderIn<encA,encB> encoder;
-  #define ENC_SENSIVITY 4
-  encoderInStream<encA,encB> encStream(encoder,ENC_SENSIVITY);
+MENU_OUTPUTS(out, MAX_DEPTH, LCD_OUT(lcd, {0, 0, 16, 2}), NONE);
+NAVROOT(nav, mainMenu, MAX_DEPTH, in, out); // the navigation root object
 
-  keyMap encBtn_map[]={{-encBtn,defaultNavCodes[enterCmd].ch}};
-  keyIn<1> encButton(encBtn_map);
-
-  serialIn serial(Serial);
-
-  //input from the encoder + encoder button + serial
-  menuIn* inputsList[]={&encStream,&encButton,&serial};
-  chainStream<3> in(inputsList);//3 is the number of inputs
-
-  result doCalibration(eventMask e, prompt &item);
-
-  result showEvent(eventMask e,navNode& nav,prompt& item) {
-    Serial.print("event: ");
-    Serial.println(e);
-    return proceed;
-  }
-
-  int test=420;
-
-  result action1(eventMask e,navNode& nav, prompt &item) {
-    Serial.print("action1 event: ");
-    Serial.print(e);
-    Serial.println(", proceed menu");
-    Serial.flush();
-    return proceed;
-  }
-
-  result action2(eventMask e,navNode& nav, prompt &item) {
-    Serial.print("action2 event: ");
-    Serial.print(e);
-    Serial.println(", quiting menu.");
-    Serial.flush();
-    return quit;
-  }
-
-  int injectorMode=1;
-
-  TOGGLE(injectorMode,setInjectorMode,"Mode: ",doNothing,noEvent,noStyle//,doExit,enterEvent,noStyle
-    ,VALUE("Dose",0,doNothing,noEvent)
-    ,VALUE("Cont.",1,doNothing,noEvent)
-    ,VALUE("Toggle.",2,doNothing,noEvent)
-  );
-
-  int selTest=0;
-  SELECT(selTest,selMenu,"Select",doNothing,noEvent,noStyle
-    ,VALUE("Zero",0,doNothing,noEvent)
-    ,VALUE("One",1,doNothing,noEvent)
-    ,VALUE("Two",2,doNothing,noEvent)
-  );
-
-  int chooseTest=-1;
-  CHOOSE(chooseTest,chooseMenu,"Choose",doNothing,noEvent,noStyle
-    ,VALUE("First",1,doNothing,noEvent)
-    ,VALUE("Second",2,doNothing,noEvent)
-    ,VALUE("Third",3,doNothing,noEvent)
-    ,VALUE("Last",-1,doNothing,noEvent)
-  );
-
-  class altPrompt:public prompt {
-  public:
-    altPrompt(constMEM promptShadow& p):prompt(p) {}
-    Used printTo(navRoot &root,bool sel,menuOut& out, idx_t idx,idx_t len,idx_t) override {
-      return out.printRaw(F("special prompt!"),len);;
-    }
-  };
-
-  MENU(subMenu,"",showEvent,anyEvent,noStyle
-    ,OP("Sub1",showEvent,anyEvent)
-    ,OP("Sub2",showEvent,anyEvent)
-    ,OP("Sub3",showEvent,anyEvent)
-    ,altOP(altPrompt,"",showEvent,anyEvent)
-    ,EXIT("<Back")
-  );
-
-  char* constMEM hexDigit MEMMODE="0123456789ABCDEF";
-  char* constMEM hexNr[] MEMMODE={"0","x",hexDigit,hexDigit};
-  char buf1[]="0x11";
-
-  MENU(mainMenu,"Main menu",doNothing,noEvent,noStyle
-    ,FIELD(test,"Speed: ","RPM",0,500,10,1,doNothing,noEvent,noStyle)
-    ,OP("================",doNothing,enterEvent)
-    ,SUBMENU(setInjectorMode)
-    ,OP("Calibrate",doCalibration,enterEvent)
-    ,EXIT("<Back")
-  );
-
-  MENU_OUTPUTS(out,MAX_DEPTH
-    ,LCD_OUT(lcd,{0,0,16,2})
-    ,NONE
-  );
-  NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);//the navigation root object
-
-  result alert(menuOut& o,idleEvent e) {
-    if (e==idling) {
-      o.setCursor(0,0);
-      o.print("alert test");
-      o.setCursor(0,1);
-      o.print("[select] to continue...");
-    }
-    return proceed;
-  }
-
-result doCalibration(eventMask e, prompt &item) {
-    bool calibrationComplete = false;
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Dispense 100mL");
-    lcd.setCursor(0, 1);
-    lcd.print("Press to cancel");
-
-    if (digitalRead(encBtn) == LOW)
-    {
-      nav.idleOn(idle);
-    }
-
-    while (!calibrationComplete)
-    {
-      checkPumpInputs();
-    }
-
-    return proceed;
-  }
-
-  result idle(menuOut& o,idleEvent e) {
-    lcd.setCursor(0, 0);
-    o.print("REMIS");
-    o.setCursor(15, 0);
-    o.write(1);
-
+result alert(menuOut &o, idleEvent e)
+{
+  if (e == idling)
+  {
+    o.setCursor(0, 0);
+    o.print("alert test");
     o.setCursor(0, 1);
-    if (injectorMode == 0) {
-      o.print("Dose");
-    } else if (injectorMode == 1) {
-      o.print("Cont.");
-    } else if (injectorMode == 2) {
-      o.print("Toggle");
-    }
-    o.setCursor(10, 1);
-    o.print(test);
-    o.print("RPM");
+    o.print("[select] to continue...");
+  }
+  return proceed;
+}
 
-    return proceed;
+result doCalibration(eventMask e, prompt &item)
+{
+  bool calibrationComplete = false;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dispense 100mL");
+  lcd.setCursor(0, 1);
+  lcd.print("Press to cancel");
+
+  if (digitalRead(encBtn) == LOW)
+  {
+    nav.idleOn(idle);
   }
 
-  void drawMushrooms()
+  while (!calibrationComplete)
+  {
+    checkPumpInputs();
+  }
+
+  return proceed;
+}
+
+result idle(menuOut &o, idleEvent e)
+{
+  lcd.setCursor(0, 0);
+  o.print("REMIS");
+  o.setCursor(15, 0);
+  o.write(1);
+
+  o.setCursor(0, 1);
+  if (injectorMode == 0)
+  {
+    o.print("Dose");
+  }
+  else if (injectorMode == 1)
+  {
+    o.print("Cont.");
+  }
+  else if (injectorMode == 2)
+  {
+    o.print("Toggle");
+  }
+  o.setCursor(10, 1);
+  o.print(test);
+  o.print("RPM");
+
+  return proceed;
+}
+
+void drawMushrooms()
 {
   lcd.setCursor(0, 1);
   for (int i = 0; i < 16; i++)
@@ -265,18 +252,16 @@ void runPump()
 {
   if (pumpRunning == false)
   {
-    displayPumpRunning();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pump Running...");
+    lcd.setCursor(0, 1);
+    lcd.print(getRPM());
+    lcd.print(" RPM");
     pumpRunning = true;
-    dashboardDisplayed = false;
-    attachInterrupt(digitalPinToInterrupt(TOGGLESWITCH), stopPumpISR, CHANGE); // attach interrupt to TOGGLESWITCH pin
-    attachInterrupt(digitalPinToInterrupt(TRIGGER), stopPumpISR, CHANGE); // attach interrupt to TRIGGER pin
-
   }
 
-  while (pumpRunning)
-  {
-    pumpStep();
-  }
+  pumpStep();
 }
 
 void pumpStep()
@@ -301,7 +286,7 @@ void stopPump()
     pumpRunning = false;
     dashboardDisplayed = true;
     detachInterrupt(digitalPinToInterrupt(TOGGLESWITCH)); // detach the interrupt when not needed
-    detachInterrupt(digitalPinToInterrupt(TRIGGER)); // detach the interrupt when not needed
+    detachInterrupt(digitalPinToInterrupt(TRIGGER));      // detach the interrupt when not needed
   }
 }
 
@@ -347,42 +332,44 @@ void debugMode()
   }
 }
 
-  void setup() 
+void setup()
+{
+  pinMode(encBtn, INPUT_PULLUP);
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  encoder.begin();
+  lcd.begin(16, 2);
+  nav.idleTask = idle; // point a function to be used when menu is suspended
+  mainMenu[1].enabled = disabledStatus;
+  nav.showTitle = false;
+
+  lcd.createChar(0, mushroom);
+  lcd.createChar(1, batteryIcon);
+  lcd.setCursor(0, 0);
+  lcd.print("REMIS       v0.5");
+  lcd.setCursor(0, 12);
+  drawMushrooms();
+
+  nav.idleOn(idle);
+
+  step_delay_microseconds = 500;
+}
+
+void showDashboard()
+{
+  if (!dashboardDisplayed)
   {
-    pinMode(encBtn,INPUT_PULLUP);
-    Serial.begin(115200);
-    while(!Serial);
-    encoder.begin();
-    lcd.begin(16,2);
-    nav.idleTask=idle;//point a function to be used when menu is suspended
-    mainMenu[1].enabled=disabledStatus;
-    nav.showTitle=false;
-
-    lcd.createChar(0, mushroom);
-    lcd.createChar(1, batteryIcon);
-    lcd.setCursor(0, 0);
-    lcd.print("REMIS       v0.5");
-    lcd.setCursor(0, 12);
-    drawMushrooms();
-
     nav.idleOn(idle);
-
-    step_delay_microseconds = 500;
+    dashboardDisplayed = true;
   }
+}
 
-  void showDashboard()
-  {
-    if (!dashboardDisplayed)
-    {
-      nav.idleOn(idle);
-      dashboardDisplayed = true;
-    }
-  }
+void loop()
+{
+  nav.poll();
 
-  void loop() {
-    nav.poll();
-
-    // if the rotary encoder button is held for 5 seconds, enter debug mode
+  // if the rotary encoder button is held for 5 seconds, enter debug mode
   if (digitalRead(ROT_SW) == LOW)
   {
     int i = 0;
@@ -390,7 +377,6 @@ void debugMode()
     {
       if (digitalRead(ROT_SW) == HIGH)
       {
-        break;
       }
       delay(10);
       i++;
@@ -401,10 +387,44 @@ void debugMode()
     }
   }
 
-    if (!checkPumpInputs())
+  if (digitalRead(TOGGLESWITCH) == HIGH)
+  {
+    runPump();
+  }
+  else if (digitalRead(TRIGGER) == HIGH && injectorMode == 0)
+  {
+    runPump();
+  }
+  else if (digitalRead(TRIGGER) == HIGH && injectorMode == 1)
+  {
+    int steps = 0;
+    int doseSteps = dose * ml_per_rev;
+    while (steps < doseSteps)
     {
-      showDashboard();
+      runPump();
+      steps++;
     }
   }
+  else
+  {
+    digitalWrite(STEPPER_STEP, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    if (pumpRunning == true)
+    {
+      lcd.clear();
+      showDashboard();
+      pumpRunning = false;
+    }
+  }
+  if (!pumpRunning)
+  {
+    showDashboard();
+  }
+
+  // if (!checkPumpInputs())
+  // {
+  //   showDashboard();
+  // }
+}
 
 #endif
