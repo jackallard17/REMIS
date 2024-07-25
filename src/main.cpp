46ffe8df881787ac4afbe1758c85654079d58c4f
Jack Allard <jackallard17@gmail.com>
2024-07-25 01:38:36 -0700

    migrate to lcdmenu library

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,63 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>   //F. Malpartida LCD's driver
-#include <menu.h>                //menu macros and objects
-#include <menuIO/lcdOut.h>       //malpartidas lcd menu output
-#include <menuIO/serialIn.h>     //Serial input
-#include <menuIO/encoderIn.h>    //quadrature encoder driver and fake stream
-#include <menuIO/keyIn.h>        //keyboard driver and fake stream (for the encoder button)
-#include <menuIO/chainStream.h>  // concatenate multiple input streams (this allows adding a button to the encoder)
-#include <Stepper.h>
+/*
+ Basic Menu
 
-// globals
+ https://lcdmenu.forntoh.dev/examples/basic
+
+*/
+
+#include <LcdMenu.h>
+#include <utils/commandProccesors.h>
+#include <Encoder.h>
+
+Encoder encoder(2, 3);
+
+// rotary encoder bindings
+#define ROT_CLK 2 // rotary encoder clock
+#define ROT_DT 3  // rotary encoder direction
+#define ROT_SW 4  // rotary encoder switch (press in)
+
+// pump input bindings
+#define TOGGLE 8
+#define TRIGGER 9
+
+// stepper motor bindings
 #define STEPPER_STEP 5
-#define STEPPER_DIR 6
+#define STEPPER_DIR 9
 
-// Digital pins D2 - D7
-#define ROT_CLK 2  // rotary encoder clock
-#define ROT_DT 3   // rotary encoder direction
-#define ROT_SW 4   // rotary encoder switch (press in)
-
+// lcd bindings
 #define LCD_SDA 22
 #define LCD_SDL 21
 
-#define TOGGLESWITCH 7
-#define TRIGGER 8
-
 #define STEPS 200
 
-#define MAX_DEPTH 2
+// menu bindings
+#define LCD_ROWS 2
+#define LCD_COLS 16
 
-volatile bool pumpRunning = false;
-volatile bool dashboardDisplayed = true;
-volatile bool frequencyUpdated = false;
+// Configure keyboard keys (ASCII)
+#define UP 56     // NUMPAD 8
+#define DOWN 50   // NUMPAD 2
+#define ENTER 53  // NUMPAD 5
+#define BACK 55   // NUMPAD 7
 
-float period;
-long step_delay_microseconds;
-
-float ml_per_rev = 200;
-
-Stepper stepper(STEPS, STEPPER_DIR, STEPPER_STEP);
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
-// persistent properties, stored between power cycles
-float frequency;  // frequency of the pump in Hz
-int dose;         // dose amount in mL
-
-uint8_t mushroom[8] = {
-  0b00000,
-  0b00100,
-  0b01110,
-  0b11111,
-  0b00100,
-  0b00100,
-  0b00100,
-  0b00100
-};
-
-byte batteryIcon[8] = {
-  B00100,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B01110,
-  B00000
-};
-
+// Initialize the main menu items
+MAIN_MENU(
+    ITEM_BASIC("Injector Mode"),
+    ITEM_BASIC("Calibrate"),
+    ITEM_BASIC("Pump Speed")
+);
+// Construct the LcdMenu
+LcdMenu menu(LCD_ROWS, LCD_COLS);
 
 void setup() {
+    Serial.begin(9600);
+    // Initialize LcdMenu with the menu items
+    menu.setupLcdWithMenu(0x27, mainMenu);
 }
 
 void loop() {
-  lcd.createChar(0, mushroom);
-  lcd.createChar(1, batteryIcon);
-  lcd.setCursor(0, 0);
-  lcd.print("REMIS       v0.5");
-  lcd.setCursor(0, 12);
-  //drawMushrooms();
+    if (!Serial.available()) return;
+    char command = Serial.read();
+
+    processMenuCommand(menu, command, UP, DOWN, ENTER, BACK);
 }
