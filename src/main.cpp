21b4307bff923124b0573e747179fae5b42fc4fe
Jack Allard <jackallard17@gmail.com>
2024-08-06 12:22:22 -0700

    re-extract pin defs into header in src

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,12 @@
 #include <Encoder.h>
 #include <EZButton.h>
 
+#include <Pins.h>
+#include <PumpController/PumpController.h>
+
 Encoder encoder(2, 3);
 
-// rotary encoder bindings
-#define ROT_CLK 2 // rotary encoder clock
-#define ROT_DT 3  // rotary encoder direction
-#define ROT_SW 4  // rotary encoder switch (press in)
-
-// pump input bindings
-//#define TOGGLE 7
-#define TRIGGER 8
-
-// stepper motor bindings
-#define STEPPER_STEP 5
-#define STEPPER_DIR 6
-
-// lcd bindings
-#define LCD_SDA 22
-#define LCD_SDL 21
-
 LiquidCrystal_I2C lcd(0x27, 2, 16);
-
-#define STEPS 200
-
-// menu bindings
-#define LCD_ROWS 2
-#define LCD_COLS 16
-
-// Configure keyboard keys (ASCII)
-#define UP 56     // NUMPAD 8
-#define DOWN 50   // NUMPAD 2
-#define ENTER 53  // NUMPAD 5
-#define BACK 55   // NUMPAD 7
 
 MAIN_MENU(
     ITEM_BASIC("Injector Mode"),
